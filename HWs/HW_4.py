d47b1ba83dcd25c976406df2ca7fe00e2d628a80
msusaes <norton18@msu.edu>
2023-10-10 20:09:44 -0400

    progress on Project 1

--- HWs/HW_4.py
+++ HWs/HW_4.py
@@ -5,7 +5,7 @@ import time
 if "file" not in st.session_state:
     st.session_state["file"] = "not done"
 
-col1, col2 = st.columns([1,2])
+col1, col2 = st.columns([1,2])#
 col1.markdown("# Welcome to my app! ")
 col1.markdown(" Here is some info on the app")
 
--- Midterm Project/Project_1.py
+++ Midterm Project/Project_1.py
@@ -3,10 +3,9 @@ import altair as alt
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
-st.title('Annual Emissions by Country')
 
-data_load_state = st.text('Loading data...')
-df = pd.read_csv("archive/GCB2022v27_MtCO2_flat.csv")
+st.set_page_config(layout="wide")
+df = pd.read_csv("Emissions/GCB2022v27_MtCO2_flat.csv")
 df = df.set_index("Country")
 
 latLon = pd.read_csv("https://gist.githubusercontent.com/tadast/8827699/raw/f5cac3d42d16b78348610fc4ec301e9234f82821/countries_codes_and_coordinates.csv")
@@ -23,23 +22,57 @@ df2 = df.merge(latLon, how="left", on="Alpha-3 code")
 df2["Country"] = df.reset_index()["Country"]
 df2.set_index("Country")
 
-data_load_state.text('Loading data...done!')
+
+col1, col2, col3 = st.columns(3)
+# col1.title('Country Emissions')
+
+# col2.title('Annual Total Emissions')
+
+# col3.title('Emissions by Type')
+
+st.subheader('Raw data')
+st.write(df)
+
+types = col1.selectbox("Choose emission type", list(df.columns[2:8]))
+
+start = col1.number_input("Please enter a start date", min(df["Year"]), max(df["Year"]))
+end = col1.number_input("Please enter an end date", min(df["Year"]), max(df["Year"]))
+if start > end:
+    col2.error("Start date cannot be later than end date")
+
+
+countries = col1.multiselect(
+    "Choose countries", list(df.index.unique()), ["China", "USA"]
+)
+if not countries:
+    col1.error("Please select at least one country.")
+else:
+    data = df2.set_index("Country")
+    data = data.loc[countries]
+    # fig = px.scatter_mapbox(data, lat="Latitude (average)", lon="Longitude (average)", hover_name="Alpha-3 code", hover_data='Total', zoom=1)
+    # fig.update_layout(mapbox_style="open-street-map")
+    # fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+dates = list(range(start, end+1))
+fig, ax = plt.subplots(figsize=(11, 9))
+for i in countries:
+    df_copy = df.loc[df["Country"]==i]
+
+# fig = plt.plot(dates, df[types])
+
+
+# data_load_state = st.text('Loading data...')
+# df = pd.read_csv("Emissions/GCB2022v27_MtCO2_flat.csv")
+# df = df.set_index("Country")
+
+# #data_load_state.text('Loading data...done!')
 
 # st.subheader('Raw data')
 # st.write(df)
 
-countries = st.multiselect(
-    "Choose countries", list(df.index.unique()), ["China", "USA"]
-)
-if not countries:
-    st.error("Please select at least one country.")
-else:
-    data = df2.set_index("Country")
-    data = data.loc[countries]
-    fig = px.scatter_mapbox(data, lat="Latitude (average)", lon="Longitude (average)", hover_name="Alpha-3 code", hover_data='Total', zoom=1, height=400)
-    fig.update_layout(mapbox_style="open-street-map")
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    st.plotly_chart(fig)
+
+
+    # col1.plotly_chart(fig)
     # fig, x = plt.subplots()
     # for index, row in data.iterrows():
     #     x.plot(row, label=index)
